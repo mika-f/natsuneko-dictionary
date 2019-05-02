81f6dffde405318d42d53f8c1adb50311958f775
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-05-02 22:45:08 +0900

    fix(strict): use utf8

--- lib/Kaomojic/Strict.pm
+++ lib/Kaomojic/Strict.pm
@@ -1,10 +1,12 @@
 package Kaomojic::Strict;
 use strict;
 use warnings;
+use utf8;
 use parent qw/Import::Base/;
 
 our @IMPORT_MODULES = (
     'strict',
     'warnings',
+    'utf8',
     'feature' => [qw/:5.28/]
 );
